aa91be17ad5f2d880e08644d909423b338fa4b5a
venu630 <sirisanagandlavenu@gmail.com>
2022-11-13 14:10:39 +0530

    thirdd commit

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle("Swagger Test")
-  .setDescription("This is a test Swagger")
+  .setTitle("Swagger Email Verification")
+  .setDescription("Swagger Test for Email Verification")
   .setVersion("1.0")
   .build();
 
